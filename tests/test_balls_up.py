6e6472dde95a437dc3e4b043ddadc710b6edc331
314Reactor <34923578+LordofBone@users.noreply.github.com>
2023-12-19 00:05:58 +0000

    Tests added

--- tests/test_balls_up.py
+++ tests/test_balls_up.py
@@ -2,7 +2,7 @@ import unittest
 
 from run import calculate_impact_force, linear_search_simulation_with_flag, \
     precise_halving_strategy_simulation_with_flag, binary_search_strategy, find_most_efficient_floor_from_results, \
-    cumulative_height
+    cumulative_height, run_simulation_with_adjusted_parameters, find_floor_with_most_breaks
 
 
 class TestBallDropSimulation(unittest.TestCase):
@@ -22,8 +22,8 @@ class TestBallDropSimulation(unittest.TestCase):
     def test_linear_search_simulation_with_flag_low_break(self):
         # Set up a test case
         floor_heights = [1 for _ in range(100)]  # All floors are 1 meter high
-        ball_weight = 1  # 1 kg
-        plate_strength = 1  # Can withstand a force of 10 Newtons
+        ball_weight = 0.91  # 0.91 kg
+        plate_strength = 4  # Can withstand a force of 4 Newtons
         start_floor = 50
 
         expected_attempts = 51
@@ -36,8 +36,8 @@ class TestBallDropSimulation(unittest.TestCase):
     def test_precise_halving_strategy_simulation_with_flag_low_break(self):
         # Set up a test case
         floor_heights = [1 for _ in range(100)]  # All floors are 1 meter high
-        ball_weight = 1  # 1 kg
-        plate_strength = 1  # Can withstand a force of 1 Newtons
+        ball_weight = 0.91  # 0.91 kg
+        plate_strength = 4  # Can withstand a force of 4 Newtons
         start_floor = 50
 
         expected_attempts = 7
@@ -51,8 +51,8 @@ class TestBallDropSimulation(unittest.TestCase):
     def test_binary_search_strategy_low_break(self):
         # Set up a test case
         floor_heights = [1 for _ in range(100)]  # All floors are 1 meter high
-        ball_weight = 1  # 1 kg
-        plate_strength = 1  # Can withstand a force of 10 Newtons
+        ball_weight = 0.91  # 0.91 kg
+        plate_strength = 4  # Can withstand a force of 4 Newtons
         start_floor = 50
 
         expected_attempts = 50
@@ -151,8 +151,8 @@ class TestBallDropSimulation(unittest.TestCase):
     def test_linear_search_simulation_with_flag_consistent_break_floor_low(self):
         # Set up a test case
         floor_heights = [1 for _ in range(100)]  # All floors are 1 meter high
-        ball_weight = 1  # 1 kg
-        plate_strength = 1  # Can withstand a force of 1 Newtons
+        ball_weight = 0.91  # 0.91 kg
+        plate_strength = 4  # Can withstand a force of 4 Newtons
 
         expected_did_break = True
         expected_break_floor = 1
@@ -170,8 +170,8 @@ class TestBallDropSimulation(unittest.TestCase):
     def test_precise_halving_strategy_simulation_with_flag_consistent_break_floor_low(self):
         # Set up a test case
         floor_heights = [1 for _ in range(100)]  # All floors are 1 meter high
-        ball_weight = 1  # 1 kg
-        plate_strength = 1  # Can withstand a force of 1 Newtons
+        ball_weight = 0.91  # 0.91 kg
+        plate_strength = 4  # Can withstand a force of 4 Newtons
 
         expected_did_break = True
         expected_break_floor = 1
@@ -189,8 +189,8 @@ class TestBallDropSimulation(unittest.TestCase):
     def test_binary_search_strategy_consistent_break_floor_low(self):
         # Set up a test case
         floor_heights = [1 for _ in range(100)]  # All floors are 1 meter high
-        ball_weight = 1  # 1 kg
-        plate_strength = 1  # Can withstand a force of 1 Newtons
+        ball_weight = 0.91  # 0.91 kg
+        plate_strength = 4  # Can withstand a force of 4 Newtons
 
         expected_did_break = True
         expected_break_floor = 1
@@ -362,7 +362,7 @@ class TestBallDropSimulation(unittest.TestCase):
                          binary_search_strategy(floor_heights, ball_weight, plate_strength, start_floor)
                          )
 
-    def test_find_most_efficient_floor_from_results_no_possible_break_skip(self):
+    def test_find_most_efficient_floor_from_results(self):
         simulation_results = \
             {1: {'attempts': 7465,
                  'average_attempts': 7.465,
@@ -411,6 +411,114 @@ class TestBallDropSimulation(unittest.TestCase):
         self.assertEqual((expected_floor, expected_efficiency_score),
                          find_most_efficient_floor_from_results(simulation_results)
                          )
+
+    def test_full_simulation_low(self):
+        # Set up a test case
+        num_iterations = 1  # Number of iterations
+        ball_weight_range = (0.91, 0.91)  # Ball weight
+        plate_strength_range = (4, 4)  # Plate strength
+        floor_height_range = (1, 1)  # Floor height
+
+        # List of strategies
+        strategies = [linear_search_simulation_with_flag, precise_halving_strategy_simulation_with_flag,
+                      binary_search_strategy]
+
+        # Run the simulation
+        simulation_results = run_simulation_with_adjusted_parameters(num_iterations, ball_weight_range,
+                                                                     plate_strength_range,
+                                                                     floor_height_range, strategies)
+
+        # Calculate the floor with the most breaks and its number of breaks
+        most_breaks_floor, most_breaks = find_floor_with_most_breaks(simulation_results)
+
+        # Calculate the most efficient floor and its efficiency score
+        most_efficient_floor, efficiency_score = find_most_efficient_floor_from_results(simulation_results)
+
+        self.assertEqual(1, most_breaks_floor)
+        self.assertEqual(300, most_breaks)
+        self.assertEqual(1, most_efficient_floor)
+        self.assertEqual(0.6012777777777779, efficiency_score)
+
+    def test_full_simulation_mid(self):
+        # Set up a test case
+        num_iterations = 1  # Number of iterations
+        ball_weight_range = (1, 1)  # Ball weight
+        plate_strength_range = (31, 31)  # Plate strength
+        floor_height_range = (1, 1)  # Floor height
+
+        # List of strategies
+        strategies = [linear_search_simulation_with_flag, precise_halving_strategy_simulation_with_flag,
+                      binary_search_strategy]
+
+        # Run the simulation
+        simulation_results = run_simulation_with_adjusted_parameters(num_iterations, ball_weight_range,
+                                                                     plate_strength_range,
+                                                                     floor_height_range, strategies)
+
+        # Calculate the floor with the most breaks and its number of breaks
+        most_breaks_floor, most_breaks = find_floor_with_most_breaks(simulation_results)
+
+        # Calculate the most efficient floor and its efficiency score
+        most_efficient_floor, efficiency_score = find_most_efficient_floor_from_results(simulation_results)
+
+        self.assertEqual(50, most_breaks_floor)
+        self.assertEqual(300, most_breaks)
+        self.assertEqual(50, most_efficient_floor)
+        self.assertEqual(0.6251666666666666, efficiency_score)
+
+    def test_full_simulation_high(self):
+        # Set up a test case
+        num_iterations = 1  # Number of iterations
+        ball_weight_range = (0.994, 0.994)  # Ball weight
+        plate_strength_range = (44, 44)  # Plate strength
+        floor_height_range = (1, 1)  # Floor height
+
+        # List of strategies
+        strategies = [linear_search_simulation_with_flag, precise_halving_strategy_simulation_with_flag,
+                      binary_search_strategy]
+
+        # Run the simulation
+        simulation_results = run_simulation_with_adjusted_parameters(num_iterations, ball_weight_range,
+                                                                     plate_strength_range,
+                                                                     floor_height_range, strategies)
+
+        # Calculate the floor with the most breaks and its number of breaks
+        most_breaks_floor, most_breaks = find_floor_with_most_breaks(simulation_results)
+
+        # Calculate the most efficient floor and its efficiency score
+        most_efficient_floor, efficiency_score = find_most_efficient_floor_from_results(simulation_results)
+
+        self.assertEqual(100, most_breaks_floor)
+        self.assertEqual(300, most_breaks)
+        self.assertEqual(100, most_efficient_floor)
+        self.assertEqual(0.8539999999999999, efficiency_score)
+
+    def test_full_simulation_no_break(self):
+        # Set up a test case
+        num_iterations = 1  # Number of iterations
+        ball_weight_range = (0.993, 0.993)  # Ball weight
+        plate_strength_range = (44, 44)  # Plate strength
+        floor_height_range = (1, 1)  # Floor height
+
+        # List of strategies
+        strategies = [linear_search_simulation_with_flag, precise_halving_strategy_simulation_with_flag,
+                      binary_search_strategy]
+
+        # Run the simulation
+        simulation_results = run_simulation_with_adjusted_parameters(num_iterations, ball_weight_range,
+                                                                     plate_strength_range,
+                                                                     floor_height_range, strategies)
+
+        # Calculate the floor with the most breaks and its number of breaks
+        most_breaks_floor, most_breaks = find_floor_with_most_breaks(simulation_results)
+
+        # Calculate the most efficient floor and its efficiency score
+        most_efficient_floor, efficiency_score = find_most_efficient_floor_from_results(simulation_results)
+
+        self.assertEqual(None, most_breaks_floor)
+        self.assertEqual(0, most_breaks)
+        self.assertEqual(1, most_efficient_floor)
+        self.assertEqual(float('inf'), efficiency_score)
 
 
 if __name__ == '__main__':
